a2c4e7653a45dd6a4ce67d08d437c2c181eaa1e2
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2023-11-23 22:21:02 +1100

    ✨ Wire up initial confluence functions

--- confluence_space_cloner/main.py
+++ confluence_space_cloner/main.py
@@ -1,5 +1,30 @@
-def main():
-    print("Hello World!")
+from dotenv import load_dotenv
+
+from lib import confluence
+from lib.config import Config, get_config
+
+
+def main(config: Config):
+    # Get source space
+    source_space = confluence.get_space(
+        confluence_domain=config.confluence_domain,
+        username=config.username,
+        api_token=config.api_token,
+        space_id="abc123",
+    )
+
+    print(f"Source space: {source_space}")
+
+    # Create destination space
+    confluence.create_space(
+        api_token=config.api_token,
+        confluence_domain=config.confluence_domain,
+        space_id="xyz123",
+        space_name="Cloned space name",
+    )
+
 
 if __name__ == "__main__":
-    main()
+    load_dotenv()
+    config = get_config()
+    main(config=config)
